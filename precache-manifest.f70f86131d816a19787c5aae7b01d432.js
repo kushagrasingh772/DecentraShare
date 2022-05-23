de8cfad6e228b713bf18611fe748e6ba634ab9f6
Kushagra Singh <kushagrasingh772@gmail.com>
2022-05-23 20:03:20 +0530

    Updates

---
+++ precache-manifest.f70f86131d816a19787c5aae7b01d432.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1a67f1e77aac66ff10a31f1df4e8c45",
+    "url": "/DecentraShare/index.html"
+  },
+  {
+    "revision": "aa73b84d704be8bb8257",
+    "url": "/DecentraShare/static/css/2.5dcc52c6.chunk.css"
+  },
+  {
+    "revision": "1eccbe6f72e18ad26f6d",
+    "url": "/DecentraShare/static/css/main.e83d913d.chunk.css"
+  },
+  {
+    "revision": "aa73b84d704be8bb8257",
+    "url": "/DecentraShare/static/js/2.2e37fbca.chunk.js"
+  },
+  {
+    "revision": "30545a24812bc2557c4d652dede4cf2a",
+    "url": "/DecentraShare/static/js/2.2e37fbca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1eccbe6f72e18ad26f6d",
+    "url": "/DecentraShare/static/js/main.d3582a5a.chunk.js"
+  },
+  {
+    "revision": "d90ff555810abf8ced6b",
+    "url": "/DecentraShare/static/js/runtime-main.a29db401.js"
+  },
+  {
+    "revision": "d6cbd8e6d17d679dd3cd7fe3884ac8bb",
+    "url": "/DecentraShare/static/media/photo.d6cbd8e6.png"
+  }
+]);
